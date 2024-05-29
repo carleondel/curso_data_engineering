306be9ca18d7352f2cecf011be8e549cd7201bb3
Carlos León <carleondel@gmail.com>
2024-05-29 16:29:10 +0000

    status(promo) dimension table created & generic tests created and passed

--- models/staging/_sql_server_sources/stg_sql_server_dbo__status_dim.sql
+++ models/staging/_sql_server_sources/stg_sql_server_dbo__status_dim.sql
@@ -1,10 +1,9 @@
-
 select 
-    0 AS status_id
+    0 AS status_id,
     'Inactive' AS status_name
 UNION ALL
 select 
-    1 AS status_id
+    1 AS status_id,
     'Active' AS status_name
 
 /*esta tabla es interesante para simplificar las consultas despues de la tabla promo*/
